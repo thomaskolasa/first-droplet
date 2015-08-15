c445001ca19f2c665a1c759fd9790bf17788423c
Thomas Kolasa <tkolasa2028@gmail.com>
2015-08-15 15:51:56 -0400

    fixed app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var ejs = require('ejs');
+
+app.set('view_engine', 'ejs');
 
 
 app.get('/', function(req, res){
-  res.render('test.html');
+  res.render('test.html.ejs');
 });
 
 app.listen(3000, function(){
